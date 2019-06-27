62830e3a01632560d5c1be03a6fa6231837ea18f
G M <gremcleo@gmail.com>
2019-06-27 16:57:06 -0400

    reconfigure react-router and remove switch component

--- tsmcleod/src/components/App.js
+++ tsmcleod/src/components/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import { StripeProvider } from "react-stripe-elements";
-import Home from "./Home";
-import About from "./About";
-import Furniture from "./Furniture";
-import Shop from "./Shop";
-import Contact from "./Contact";
-import Header from "./Header";
-import Foot from "./Foot";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <script src="https://js.stripe.com/v3/" />
-
-        <Header />
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/furniture" component={Furniture} />
-            <Route exact path="/Shop" component={Shop} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/contact" component={Contact} />
-          </Switch>
+      <div className="flex justify-center pa3 " id="fade">
+        <span style={{ height: "500px" }} />
+        <div className="example">
+          <h1>Under Construction :)</h1>
         </div>
-        <Foot />
       </div>
     );
   }
--- tsmcleod/src/index.js
+++ tsmcleod/src/index.js
@@ -2,15 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "tachyons/css/tachyons.css";
 import "./index.css";
+import { Route, BrowserRouter } from "react-router-dom";
 
 import App from "./components/App";
+import About from "./components/About";
+import Furniture from "./components/Furniture";
+import Shop from "./components/Shop";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Foot from "./components/Foot";
 //import register from './serviceWorker';
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const routing = (
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+    <Header />
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/furniture" component={Furniture} />
+      <Route exact path="/shop" component={Shop} />
+      <Route exact path="/contact" component={Contact} />
+    </div>
+    <Foot />
+  </BrowserRouter>
 );
+//ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(routing, document.getElementById("root"));
 //register();
