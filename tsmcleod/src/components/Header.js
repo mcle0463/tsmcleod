52fefcf1ca05d67216e0bb6b4c4cea0706093395
G M <gremcleo@gmail.com>
2019-07-09 13:51:11 -0400

    render nav/header based on screen size, move html out of Header component

--- tsmcleod/src/components/Header.js
+++ tsmcleod/src/components/Header.js
@@ -1,68 +1,24 @@
 import React, { Component } from "react";
-import Banner from "./Banner";
 import { withRouter } from "react-router";
-import "../index.css";
-import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import Nav from "./Nav";
 
 class Header extends Component {
   render() {
-    return (
-      <header className="fl w-20 fixed dn db-l">
-        {/* <div className="w-25 pa3 mr6">
-          <Link className="link dim black f3 " to="/">
-            TSMCLEOD
-          </Link>
-        </div>
-
-        <div className="flex flex-wrap w-50 pa3">
-          {/*<Link className="link dim black f6 pv1 mh3" to="/">
-            HO
-          </Link>
-          <Link
-            className="link dim black f6 dib pv1 mh3"
-            to="about"
-            title="About"
-          >
-            About Me
-          </Link>
-
-          <Link
-            className="link dim black f6 dib  pv1 mh3"
-            to="Gallery"
-            title="Gallery"
-          >
-            Gallery
-          </Link>
-          <Link
-            className="link dim black f6 dib  pv1 mh3"
-            to="Shop"
-            title="Store"
-          >
-            Shop
-          </Link>
-          <Link
-            className="link dim black f6 dib pv1 mh3"
-            to="Contact"
-            title="Contact"
-          >
-            Contact Me
-          </Link>
-        </div>
-        <input
-          className="db hover-black bg-transparent pointer ba b--black-20 pl3 pr3 fr"
-          type="submit"
-          value="Cart"
-        />*/}
-        <div className="fl dt vh-100">
-          <div className="v-mid dtc">
-            <Link className="link dim black f3 " to="/">
-              <p className="banner-text h5">TESSA S. MCLEOD</p>
-            </Link>
-          </div>
-        </div>
-        <Banner />
-      </header>
-    );
+    let width = window.innerWidth;
+    if (width > 480) {
+      return (
+        <header>
+          <Nav />
+        </header>
+      );
+    } else {
+      return (
+        <header>
+          <MobileNav />
+        </header>
+      );
+    }
   }
 }
 
--- tsmcleod/src/index.js
+++ tsmcleod/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "tachyons/css/tachyons.css";
-import "./index.css";
 import Foot from "./components/Foot";
 import { Route, BrowserRouter } from "react-router-dom";
+
+import "tachyons/css/tachyons.css";
+import "./index.css";
+import "./animate-menu.css";
 
 import App from "./components/App";
 import About from "./components/About";
@@ -11,17 +13,15 @@ import Gallery from "./components/Gallery";
 import Shop from "./components/Shop";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
-import MobileHeader from "./components/MobileHeader";
 
 //import register from './serviceWorker';
 
 const routing = (
   <BrowserRouter>
-    <MobileHeader />
     <Header />
     <div className="flex pl2 pr2">
       <div className="w-20-ns pr1-ns fl" /> {/* buffer for left header */}
-      <div className="flex-column pa-2 w-100">
+      <div className="flex-column pa2 w-100">
         {" "}
         {/* flex-column for everything other than left header*/}
         <Route exact path="/" component={App} />
